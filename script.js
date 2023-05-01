9f799cbda01d14911f1d915303e72fb876d3e8b5
Keller Lin <114589962+kellerl3641@users.noreply.github.com>
2023-05-01 21:21:19 +0000

    Mon, May 1, 2023, 9:21 PM +00:00

--- script.js
+++ script.js
@@ -21,20 +21,21 @@ $(document).ready(function(){
     } // End if
   });
 
-  $(window).scroll(function() {
-    $(".slideanim").each(function(){
-      var pos = $(this).offset().top;
-
-      var winTop = $(window).scrollTop();
-        if (pos < winTop + 600) {
-          $(this).addClass("slide");
-        }
-    });
-  });
-})
 
 jQuery(function () {
   jQuery(".emailLink").click(function () {
       alert("I hate people who don't listen.");
   });
 });
+
+$(window).scroll(function() {
+  $(".slideanim").each(function(){
+    var pos = $(this).offset().top;
+
+    var winTop = $(window).scrollTop();
+      if (pos < winTop + 600) {
+        $(this).addClass("slide");
+      }
+  });
+});
+})
